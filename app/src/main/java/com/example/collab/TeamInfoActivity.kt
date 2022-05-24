bea04697b93a5ad138f0f2de219c64e72e5487b1
Haeba <onpima@naver.com>
2022-05-25 04:43:12 +0900

    design: 팀 정보 공유 activity

---
+++ app/src/main/java/com/example/collab/TeamInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.collab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_info)
+    }
+}
