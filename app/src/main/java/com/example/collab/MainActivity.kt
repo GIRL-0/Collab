41097d909f4fac0f575db739ea750084047e3279
Haeba <onpima@naver.com>
2022-05-25 04:40:10 +0900

    chore: main activity 코드 삭제

--- app/src/main/java/com/example/collab/MainActivity.kt
+++ app/src/main/java/com/example/collab/MainActivity.kt
@@ -7,20 +7,10 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
-    val textarr = arrayListOf<String>("팀 찾기","테스트")
-    val iconarr= arrayListOf<Int>(R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initLayout()
-    }
-
-    private fun initLayout() {
-        binding.viewpager.adapter = ViewPagerAdapter(this)
-        TabLayoutMediator(binding.tablayout,binding.viewpager){
-            tab,position->
-            tab.text = textarr[position]
-        }.attach()
     }
 }
