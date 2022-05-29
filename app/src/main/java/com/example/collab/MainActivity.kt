7964694e6c1679221dc071a95ac3ed378a91b6f5
qiyana-ratchet <wereter31@naver.com>
2022-05-29 10:07:17 +0900

    cal: 달력 로드

--- app/src/main/java/com/example/collab/MainActivity.kt
+++ app/src/main/java/com/example/collab/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.collab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.collab.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -12,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initCal()
+    }
+
+    private fun initCal() {
+        with(binding){
+            calButton.setOnClickListener{
+                Toast.makeText(this@MainActivity,"눌림.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@MainActivity, PersonalCalendarActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
