4b62d7fe9e69c77df002cfd2698b39c089e142dd
qiyana-ratchet <wereter31@naver.com>
2022-05-30 20:47:01 +0900

    plan: 일정 init() 생성

--- app/src/main/java/com/example/collab/CreatePlanActivity.kt
+++ app/src/main/java/com/example/collab/CreatePlanActivity.kt
@@ -2,10 +2,21 @@ package com.example.collab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.collab.databinding.ActivityCreatePlanBinding
+import com.example.collab.databinding.ActivityPersonalCalendarBinding
 
 class CreatePlanActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCreatePlanBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_plan)
+        binding = ActivityCreatePlanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+
+    private fun init() {
+
+    }
+
 }
