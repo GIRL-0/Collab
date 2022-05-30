2ec03cd986be5715b9fa12e74b5d4142814192bf
qiyana-ratchet <wereter31@naver.com>
2022-05-30 22:15:17 +0900

    plan: 일정 init 추가

--- app/src/main/java/com/example/collab/CreatePlanActivity.kt
+++ app/src/main/java/com/example/collab/CreatePlanActivity.kt
@@ -2,21 +2,33 @@ package com.example.collab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.collab.databinding.ActivityCreatePlanBinding
 import com.example.collab.databinding.ActivityPersonalCalendarBinding
 
 class CreatePlanActivity : AppCompatActivity() {
-    lateinit var binding: ActivityCreatePlanBinding
+    lateinit var binding: ActivityPersonalCalendarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCreatePlanBinding.inflate(layoutInflater)
+        binding = ActivityPersonalCalendarBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
     }
 
     private fun init() {
-
+//        binding.personalPlanAddBtn.setOnClickListener {
+//            Toast.makeText(applicationContext, "눌림", Toast.LENGTH_SHORT).show()
+//            val dialog = CreatePlanDialog(this)
+//            dialog.showDialog()
+//            dialog.setOnClickListener(object : CreatePlanDialog.OnDialogClickListener {
+//                override fun onClicked(name: String)
+//                {
+//                    Toast.makeText(applicationContext, "완료", Toast.LENGTH_SHORT).show()
+//                }
+//
+//            })
+//        }
     }
 
 }
