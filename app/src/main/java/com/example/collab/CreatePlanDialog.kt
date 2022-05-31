9d0f445720c25643c6f6cca1839153b5a30943ce
qiyana-ratchet <wereter31@naver.com>
2022-05-31 18:20:38 +0900

    plan: 새 일정 추가 기능

--- app/src/main/java/com/example/collab/CreatePlanDialog.kt
+++ app/src/main/java/com/example/collab/CreatePlanDialog.kt
@@ -1,43 +1,52 @@
 package com.example.collab
 
+import android.app.Activity
 import android.app.Dialog
-import android.content.Context
+import android.content.Intent
 import android.view.WindowManager
-import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
 
-class CreatePlanDialog(context: PersonalCalendarActivity){
+
+class CreatePlanDialog(context: PersonalCalendarActivity) {
+    var context = this
     private val dialog = Dialog(context)
     private lateinit var onClickListener: OnDialogClickListener
+    val calendarData: ArrayList<CalendarData> = ArrayList()
 
-    fun setOnClickListener(listener: OnDialogClickListener)
-    {
+    interface OnDialogClickListener {
+        fun onClicked(name: String)
+    }
+
+    fun setOnClickListener(listener: OnDialogClickListener) {
         onClickListener = listener
     }
 
-    fun showDialog()
-    {
+    fun showDialog() {
         dialog.setContentView(R.layout.activity_create_plan)
-        dialog.window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT
+        )
         dialog.setCanceledOnTouchOutside(true)
         dialog.setCancelable(true)
         dialog.show()
 
-        val edit_name = dialog.findViewById<EditText>(R.id.planTitle)
+        dialog.findViewById<Button>(R.id.detailWorkAddBtn).setOnClickListener {
+            var planTitle = dialog.findViewById<TextView>(R.id.planTitle).text.toString()
+            var planStartTime = dialog.findViewById<TextView>(R.id.planStartTime).text.toString()
+            var planFinishTime = dialog.findViewById<TextView>(R.id.planFinishTime).text.toString()
+            /**
+             * 데이터베이스 연결
+             */
+            calendarData.add(CalendarData(planTitle, planStartTime, planFinishTime))
+            /*****************/
 
-//        dialog.binding.setOnClickListener {
-//            dialog.dismiss()
-//        }
-//
-//        dialog.finish_button.setOnClickListener {
-//            onClickListener.onClicked(edit_name.text.toString())
-//            dialog.dismiss()
-//        }
+            /*****************/
 
-    }
+            dialog.dismiss()
+        }
 
-    interface OnDialogClickListener
-    {
-        fun onClicked(name: String)
     }
 
 }
