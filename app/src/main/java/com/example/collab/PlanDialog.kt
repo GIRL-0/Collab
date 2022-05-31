bfd34720b9b658346d4adacc38bef051fd175d4b
qiyana-ratchet <wereter31@naver.com>
2022-05-31 23:23:00 +0900

    plan: 날짜 선택 기능

--- app/src/main/java/com/example/collab/PlanDialog.kt
+++ app/src/main/java/com/example/collab/PlanDialog.kt
@@ -1,24 +1,20 @@
 package com.example.collab
 
-import android.app.Activity
 import android.app.Dialog
-import android.content.Intent
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
-import java.io.File
-import java.io.FileWriter
-import java.util.*
 
 
 class PlanDialog(context: PersonalCalendarActivity) {
     var context = this
     private val dialog = Dialog(context)
-    private lateinit var onClickListener: OnDialogClickListener
+    private lateinit var onDismissedListener: onPlanCreateClickListener
 
-    fun setOnClickListener(listener: OnDialogClickListener) {
-        onClickListener = listener
+    fun onDismissedListener(listener: onPlanCreateClickListener) {
+        onDismissedListener = listener
     }
 
     fun showDialog() {
@@ -32,24 +28,32 @@ class PlanDialog(context: PersonalCalendarActivity) {
         dialog.show()
 
         dialog.findViewById<Button>(R.id.detailWorkAddBtn).setOnClickListener {
-            onClickListener.onClicked("확인")
+            onDismissedListener.onPlanCreateClick("확인")
             var planTitle = dialog.findViewById<TextView>(R.id.planTitle).text.toString()
             var planStartTime = dialog.findViewById<TextView>(R.id.planStartTime).text.toString()
             var planFinishTime = dialog.findViewById<TextView>(R.id.planFinishTime).text.toString()
-            /**
-             * 데이터베이스 연결
-             */
-            Toast.makeText(dialog.context, "데이터베이스 연결하기", Toast.LENGTH_SHORT).show()
-            dialog.dismiss()
-        }
 
-        dialog.setOnDismissListener {
-
+            if (isValidPlan(planTitle,planStartTime,planFinishTime)) {
+                //TODO: 데이터베이스에 추가하기
+                dialog.dismiss()
+            }
         }
+        dialog.findViewById<CalendarView>(R.id.calendar).setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
+            var toast = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+            Toast.makeText(dialog.context, toast, Toast.LENGTH_SHORT).show()
+            dialog.findViewById<TextView>(R.id.planStartTime).text = year.toString()+"-"+(month+1).toString()+"-"+dayOfMonth.toString()+"/12:00"
+            dialog.findViewById<TextView>(R.id.planFinishTime).text = year.toString()+"-"+(month+1).toString()+"-"+(dayOfMonth+1).toString()+"/12:00"
+        }
+//        dialog.setOnDismissListener {}
     }
 
-    interface OnDialogClickListener {
-        fun onClicked(name: String)
+    private fun isValidPlan(planTitle: String, planStartTime: String, planFinishTime: String): Boolean {
+        //TODO: isValid 만들기
+        return true;
+    }
+
+    interface onPlanCreateClickListener {
+        fun onPlanCreateClick(name: String)
     }
 
 }
