7e9fc3f901d86ec74986f68c012432d397c0e2a7
qiyana-ratchet <wereter31@naver.com>
2022-06-02 14:43:59 +0900

    profile : 어댑터 추가

--- app/src/main/java/com/example/collab/ProfileActivity.kt
+++ app/src/main/java/com/example/collab/ProfileActivity.kt
@@ -1,18 +1,46 @@
 package com.example.collab
 
+import PersonalCalendarAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.collab.databinding.ActivityProfileBinding
+import java.util.ArrayList
 
 class ProfileActivity : AppCompatActivity() {
     lateinit var binding : ActivityProfileBinding
+    lateinit var profileNoticeRecyclerView: RecyclerView
+    val profileNoticeData: ArrayList<ProfileNoticeData> = ArrayList()
     var context = this
+    lateinit var adapter: ProfileNoticeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initlayout()
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView() {
+        Toast.makeText(applicationContext, "initProfileRecyclerView()", Toast.LENGTH_SHORT).show()
+        profileNoticeRecyclerView = findViewById(R.id.profileAlarmRecyclerView)
+        profileNoticeRecyclerView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL, false
+        )
+
+        adapter = ProfileNoticeAdapter(profileNoticeData)
+        profileNoticeRecyclerView.adapter = adapter
+        adapter.itemClickListener = object : ProfileNoticeAdapter.OnItemClickListener {
+            override fun OnItemClick(data: ProfileNoticeData) {
+                Toast.makeText(applicationContext, data.alarmContent, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun initlayout() {
