143a0beed7e9dbf1923db4361a2f11f80d4b56ce
qiyana-ratchet <wereter31@naver.com>
2022-06-03 21:00:53 +0900

    login: 로그아웃 기능 추가

--- app/src/main/java/com/example/collab/LoginActivity.kt
+++ app/src/main/java/com/example/collab/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.example.collab
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.collab.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.SignInAccount
 import com.google.android.gms.common.SignInButton
@@ -15,11 +20,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     var context = this
-
+    private var googleSignInClient: GoogleSignInClient? = null
     private lateinit var auth: FirebaseAuth
 
     //    private lateinit var client: GoogleSignInClient
@@ -35,22 +41,22 @@ class LoginActivity : AppCompatActivity() {
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
         val signInGoogleBtn: SignInButton = findViewById(R.id.googleLoginBtn)
 
         signInGoogleBtn.setOnClickListener {
-            val signInIntent = googleSignInClient.signInIntent
+            val signInIntent = googleSignInClient!!.signInIntent
             startActivityForResult(signInIntent, 9001)
         }
+
+//        val logoutBtn = findViewById<TextView>(R.id.titleTextView)
+//        logoutBtn.setOnClickListener {
+//            firebaseAuthSignOut()
+//        }
+
     }
 
-    //        client = GoogleSignIn.getClient(this, options)
-//
-//        login_button.setOnClickListener {
-//            // 로그인 요청
-//            startActivityForResult(client.signInIntent, 1004)
-//        }
-//    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -74,10 +80,15 @@ class LoginActivity : AppCompatActivity() {
                     val email = auth.currentUser?.email
                     val name = auth.currentUser?.displayName
                     val photoUrl = auth.currentUser?.photoUrl
-                    Toast.makeText(applicationContext,"Login Success with ${email.toString()} ${name.toString()}",
-                        Toast.LENGTH_SHORT).show()
-                    Toast.makeText(applicationContext,"${name.toString()}님 환영합니다",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        applicationContext,
+                        "Login Success with ${email.toString()} ${name.toString()}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Toast.makeText(
+                        applicationContext, "${name.toString()}님 환영합니다",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     Log.d("googleLogin", user.toString())
 
                     var intent = Intent(context, SearchTeamActivity::class.java)
@@ -90,6 +101,10 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    private fun firebaseAuthSignOut() {
+        Firebase.auth.signOut()
+        googleSignInClient!!.signOut()
+    }
 
 //
 //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
