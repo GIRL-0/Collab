efcedf2836b36ae0227c7cf9b706b8aaef6383d5
qiyana-ratchet <wereter31@naver.com>
2022-06-08 01:42:43 +0900

    cal: 날짜 클릭 구현

--- app/src/main/java/com/example/collab/PersonalCalendarActivity.kt
+++ app/src/main/java/com/example/collab/PersonalCalendarActivity.kt
@@ -4,6 +4,7 @@ import PersonalCalendarAdapter
 import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.split
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -25,6 +26,7 @@ class PersonalCalendarActivity : AppCompatActivity() {
     lateinit var binding: ActivityPersonalCalendarBinding
     lateinit var personalCalendarRecyclerView: RecyclerView
     val calendarData: ArrayList<CalendarData> = ArrayList()
+    val tmpData: ArrayList<CalendarData> = ArrayList()
     var context = this
     lateinit var adapter: PersonalCalendarAdapter
 
@@ -63,11 +65,51 @@ class PersonalCalendarActivity : AppCompatActivity() {
         binding.personalCalendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
             var toast = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
             Toast.makeText(applicationContext, toast, Toast.LENGTH_SHORT).show()
+
+            tmpData.clear()
+            for (data in calendarData) {
+                val selectStartYear = data.planStartDate.split("-")[0].toInt()
+                val selectStartMonth = data.planStartDate.split("-")[1].toInt()
+                val selectStartDay = data.planStartDate.split("-")[2].toInt()
+                val selectEndYear = data.planEndDate.split("-")[0].toInt()
+                val selectEndMonth = data.planEndDate.split("-")[1].toInt()
+                val selectEndDay = data.planEndDate.split("-")[2].toInt()
+
+                if (selectStartYear <= year && selectStartMonth <= month + 1 && selectStartDay <= dayOfMonth
+                    && year <= selectEndYear && month + 1 <= selectEndMonth && dayOfMonth <= selectEndDay
+                ) {
+                    tmpData.add(data)
+                }
+
+            }
+            Toast.makeText(
+                applicationContext,
+                "initCalendarRecyclerView()",
+                Toast.LENGTH_SHORT
+            ).show()
+            personalCalendarRecyclerView =
+                findViewById(R.id.personalPlanRecyclerView)
+            personalCalendarRecyclerView.layoutManager = LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL, false
+            )
+            adapter = PersonalCalendarAdapter(tmpData)
+            personalCalendarRecyclerView.adapter = adapter
+            adapter.itemClickListener =
+                object : PersonalCalendarAdapter.OnItemClickListener {
+                    override fun OnItemClick(data: CalendarData) {
+                        Toast.makeText(
+                            applicationContext,
+                            data.planContent,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 
     private fun initCalendarData() {
-            //데이터베이스에 푸쉬하고 가져오기
+        //데이터베이스에 푸쉬하고 가져오기
 //            val db = Firebase.firestore
 //            val docRef = db.collection("User").document(userInfoEmail)
 //            docRef.get()
@@ -102,25 +144,42 @@ class PersonalCalendarActivity : AppCompatActivity() {
         val docRef = db.collection("User").document(userInfoEmail)
         docRef.get()
             .addOnSuccessListener { document ->
-                if (document?.get("plans") != null){
+                if (document?.get("plans") != null) {
                     var planData = document.get("plans") as ArrayList<String>
                     for (plan in planData) {
                         val container = plan.split("!")
-                        calendarData.add(CalendarData(container[0], container[1].split("/")[0], container[1].split("/")[1],
-                            container[2].split("/")[0], container[2].split("/")[1]))
-                        Toast.makeText(applicationContext, "initCalendarRecyclerView()", Toast.LENGTH_SHORT).show()
-                        personalCalendarRecyclerView = findViewById(R.id.personalPlanRecyclerView)
+                        calendarData.add(
+                            CalendarData(
+                                container[0],
+                                container[1].split("/")[0],
+                                container[1].split("/")[1],
+                                container[2].split("/")[0],
+                                container[2].split("/")[1]
+                            )
+                        )
+                        Toast.makeText(
+                            applicationContext,
+                            "initCalendarRecyclerView()",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        personalCalendarRecyclerView =
+                            findViewById(R.id.personalPlanRecyclerView)
                         personalCalendarRecyclerView.layoutManager = LinearLayoutManager(
                             this,
                             LinearLayoutManager.VERTICAL, false
                         )
                         adapter = PersonalCalendarAdapter(calendarData)
                         personalCalendarRecyclerView.adapter = adapter
-                        adapter.itemClickListener = object : PersonalCalendarAdapter.OnItemClickListener {
-                            override fun OnItemClick(data: CalendarData) {
-                                Toast.makeText(applicationContext, data.planContent, Toast.LENGTH_SHORT).show()
+                        adapter.itemClickListener =
+                            object : PersonalCalendarAdapter.OnItemClickListener {
+                                override fun OnItemClick(data: CalendarData) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        data.planContent,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
                             }
-                        }
                     }
                     Log.d("calendarData 테스트", calendarData.toString())
                     Log.d("calendarData 테스트", calendarData[0].planContent.toString())
