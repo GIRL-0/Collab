1dc07128e35f59909abb34be3b4a3c44afdfa9a8
qiyana-ratchet <wereter31@naver.com>
2022-06-10 02:20:08 +0900

    Update PersonalCalendarAdapter.kt

--- app/src/main/java/com/example/collab/PersonalCalendarAdapter.kt
+++ app/src/main/java/com/example/collab/PersonalCalendarAdapter.kt
@@ -12,7 +12,6 @@ class PersonalCalendarAdapter (val items:ArrayList<CalendarData>):RecyclerView.A
     }
     var itemClickListener:OnItemClickListener?=null
 
-    // 아이템 뷰를 저장하는 뷰홀더 클래스
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val planContent = itemView.findViewById<TextView>(R.id.planContent)
         val planStartDate = itemView.findViewById<TextView>(R.id.planStartDate)
@@ -26,13 +25,11 @@ class PersonalCalendarAdapter (val items:ArrayList<CalendarData>):RecyclerView.A
         }
     }
 
-    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.plan_row, parent, false)
         return ViewHolder(view)
     }
 
-    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.planContent.text = items[position].planContent
         holder.planStartDate.text = items[position].planStartDate
@@ -44,17 +41,4 @@ class PersonalCalendarAdapter (val items:ArrayList<CalendarData>):RecyclerView.A
     override fun getItemCount(): Int {
         return items.size
     }
-
-    //    fun moveItem(oldPos:Int, newPos:Int){
-//        val item = items[oldPos]
-//        items.removeAt(oldPos)
-//        items.add(newPos,item)
-//        notifyItemMoved(oldPos, newPos)
-//    }
-//
-//    fun removeItem(pos:Int){
-//        items.removeAt(pos)
-//        notifyItemRemoved(pos)
-//    }
-
 }
