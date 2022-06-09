477c0f306a0677459b00a5ddbdc8cd358c07c96d
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-06-09 16:28:20 +0900

    TeamProject

--- app/src/main/java/com/example/collab/DashBoardActivity.kt
+++ app/src/main/java/com/example/collab/DashBoardActivity.kt
@@ -1,6 +1,7 @@
 package com.example.collab
 
 import PersonalCalendarAdapter
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +9,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.view.WindowManager
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.collab.databinding.ActivityDashBoardBinding
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.activity_dash_board.*
 import kotlinx.android.synthetic.main.plan_row.view.*
 import kotlinx.android.synthetic.main.work_row.view.*
@@ -56,10 +59,55 @@ class DashBoardActivity : AppCompatActivity() {
                 }.run{startActivity(this)}
             }
 
+
             val items = ArrayList<CalendarData>()
             teamPlanRecyclerView.adapter = PersonalCalendarAdapter(items)
             teamPlanRecyclerView.layoutManager = LinearLayoutManager(context)
+
+            val noticeList = ArrayList<ProfileNoticeData>()
+            teamNoticeRecyclerView.adapter = ProfileNoticeAdapter(noticeList)
+            teamNoticeRecyclerView.layoutManager = LinearLayoutManager(context)
+
             firestore = FirebaseFirestore.getInstance()
+
+            teamNoticeAddBtn.setOnClickListener {
+                val dialog = Dialog(context)
+                dialog.setContentView(R.layout.team_notice_add_dialog)
+                dialog.window!!.setLayout(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT
+                )
+                dialog.setCanceledOnTouchOutside(true)
+                dialog.setCancelable(true)
+                dialog.show()
+
+                val noticeContentEdit = dialog.findViewById<EditText>(R.id.noticeContentEdit)
+                dialog.findViewById<Button>(R.id.noticeAddBtn).setOnClickListener {
+                    firestore?.collection("Team")
+                        ?.document(teamName!!)
+                        ?.get()?.addOnSuccessListener {
+                            val str = noticeContentEdit.text.toString()
+
+                            if(it?.contains("noticeList")==true){
+                                firestore?.collection("Team")
+                                    ?.document(teamName!!)
+                                    ?.update("noticeList", FieldValue.arrayUnion(str))
+                            }else{
+                                val docData = hashMapOf(
+                                    "noticeList" to arrayListOf(str)
+                                )
+                                firestore?.collection("Team")
+                                    ?.document(teamName!!)
+                                    ?.set(docData, SetOptions.merge())
+                            }
+                        }
+                    dialog.dismiss()
+                }
+                dialog.findViewById<ImageView>(R.id.noticeAddCancelBtn).setOnClickListener {
+                    dialog.dismiss()
+                }
+            }
+
             firestore?.collection("Team")
                 ?.document(teamName!!)
                 ?.addSnapshotListener { value, error ->
@@ -75,8 +123,10 @@ class DashBoardActivity : AppCompatActivity() {
                                     container[1].split("/")[0],
                                     container[1].split("/")[1],
                                     container[2].split("/")[0],
-                                    container[2].split("/")[1])) } }
-                    teamPlanRecyclerView.adapter?.notifyDataSetChanged()
+                                    container[2].split("/")[1])) }
+                        teamPlanRecyclerView.adapter?.notifyDataSetChanged()
+                    }
+
                     if(value?.contains("todoList")==true){
                         val todoList = value?.get("todoList") as ArrayList<String>
                         firestore?.collection("Team")
@@ -99,10 +149,17 @@ class DashBoardActivity : AppCompatActivity() {
                                         progress /= progress_count
                                     totalProgressRateNum.text = progress.toString()
                                     totalProgressRate.progress = progress
-                                }
-                            }
+                                }}
                     }
 
+                    if(value?.contains("noticeList")==true){
+                        val list = value?.get("noticeList") as ArrayList<String>
+                        noticeList.clear()
+                        for(notice in list!!){
+                            noticeList.add(ProfileNoticeData(notice))
+                        }
+                        teamNoticeRecyclerView.adapter?.notifyDataSetChanged()
+                    }
 
 
                 }
