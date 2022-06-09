ff7cc89546b482e3209cef99d380903c9c24dce3
qiyana-ratchet <wereter31@naver.com>
2022-06-10 02:20:29 +0900

    Update ProfileNoticeAdapter.kt

--- app/src/main/java/com/example/collab/ProfileNoticeAdapter.kt
+++ app/src/main/java/com/example/collab/ProfileNoticeAdapter.kt
@@ -12,7 +12,6 @@ class ProfileNoticeAdapter(val items:ArrayList<ProfileNoticeData>) :RecyclerView
     }
     var itemClickListener:OnItemClickListener?=null
 
-    // 아이템 뷰를 저장하는 뷰홀더 클래스
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val alarmContent = itemView.findViewById<TextView>(R.id.noticeContent)
         init {
@@ -22,13 +21,11 @@ class ProfileNoticeAdapter(val items:ArrayList<ProfileNoticeData>) :RecyclerView
         }
     }
 
-    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.profile_notice_row, parent, false)
         return ViewHolder(view)
     }
 
-    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.alarmContent.text = items[position].alarmContent
 
